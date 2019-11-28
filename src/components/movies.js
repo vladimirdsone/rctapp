9c708a5baef155bdf866d60969f896b942a99cad
vladimirdsone <32560295+vladimirdsone@users.noreply.github.com>
2019-11-28 17:13:57 +0900

    Update movies.js

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Weather = props => (
+const Movies = props => (
     <div>
         { props.title &&
         <div>  
@@ -14,4 +14,4 @@ const Weather = props => (
         <p>{ props.error}</p>
     </div>   
 );
-export default Weather;
+export default Movies;
